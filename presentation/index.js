ac2145eee6a976db4c5e2ed272b5270aac0ffeca
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2017-02-16 19:23:41 -0800

    migrated sorting algorithm presentation

--- presentation/index.js
+++ presentation/index.js
@@ -55,115 +55,89 @@ export default class Presentation extends React.Component {
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
           <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+            <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+              Sorting Algorithms
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+            <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+              Overview and Problem Solving Techniques
+            </Text>
+            <p></p>
+            <iframe width="860" height="515" src="https://www.youtube.com/embed/kPRA0W1kECg" frameborder="0" allowfullscreen></iframe>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading size={6} textColor="primary" caps>Topics</Heading>
+            <Heading size={5} textColor="secondary">Sorting</Heading>
+            <Heading size={5} textColor="secondary">Problem Solving Techniques</Heading>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
+          <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+            <Heading size={5} textColor="secondary" caps>Sorting</Heading>
+            <Heading size={6} textColor="tertiary" caps>What is a sorting algorithm?</Heading>
+            <Appear>
+              <p>
+                "A sorting algorithm is an algorithm that puts elements of a list in a certain order." - Wikipedia
+              </p>
             </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
+            <Appear>
+              <p>
+                Goal of this week is to practice algorithmic thinking skills by implementing various types of sorting algorithms.
+              </p>
             </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
+            <Appear>
+              <p>
+                As you write your algorithms, think about why the sort you're implementing may or may not be efficient.
+                What is the time complexity of your sorting algorithm?
+              </p>
+            </Appear>
+            <Appear>
+              <Heading size={6} textColor="secondary" caps>
+                <a href="https://visualgo.net/sorting">Sorting Algorithm Visualization Tool</a>
               </Heading>
             </Appear>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
+          <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+            <Heading size={5} textColor="secondary" caps>Problem Solving Techniques</Heading>
+            <Heading size={6} textColor="tertiary" caps>General Approach</Heading>
+            <Appear>
+              <p>
+                Think carefully about what the problem is asking and try to visualize it.
+              </p>
+            </Appear>
+            <Appear>
+              <p>
+                As you're brainstorming, look for the pattern that you're trying to turn into code.
+              </p>
+            </Appear>
+            <Appear>
+              <p>
+                Use this as a guide for when you start coding.
+              </p>
+            </Appear>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
+          <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+            <Heading size={5} textColor="secondary" caps>Problem Solving Techniques</Heading>
+            <Heading size={6} textColor="tertiary" caps>Simplify and Generalize</Heading>
+            <Appear>
+              <p>
+                Oftentimes best not to go for the hail mary on the first try.
+              </p>
+            </Appear>
+            <Appear>
+              <p>
+                Solve a simplified version of the problem first.
+              </p>
+            </Appear>
+            <Appear>
+              <p>
+                For sorts, the tricky part is not necessarily switching two numbers but the way we decide which numbers to switch.
+              </p>
+            </Appear>
           </Slide>
           <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+            <BlockQuote>
+              <Quote>
+                <em>"Fin"</em>
+              </Quote>
+            </BlockQuote>
           </Slide>
         </Deck>
       </Spectacle>
